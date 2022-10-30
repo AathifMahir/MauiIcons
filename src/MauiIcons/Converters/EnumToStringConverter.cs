639bdccaf727bdd695169b7ac7b9545143eb9a29
Aathif_Mahir <aathifmahir@gmail.com>
2022-10-30 18:21:10 +0530

    Enum Converter

---
+++ src/MauiIcons/Converters/EnumToStringConverter.cs
@@ -0,0 +1,20 @@
+﻿using MauiIcons.Helpers;
+using System.Globalization;
+
+namespace MauiIcons.Converters;
+internal sealed class EnumToStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if(value != null)
+        {
+            return EnumHelper.GetEnumDescription((Enum)value);
+        }
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
