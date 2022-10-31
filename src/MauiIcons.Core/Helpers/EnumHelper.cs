9e8aecdd599974b8f8da1ba908b197801bc07a66
Aathif_Mahir <aathifmahir@gmail.com>
2022-10-31 18:17:19 +0530

    New Maui Icons Core

---
+++ src/MauiIcons.Core/Helpers/EnumHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace MauiIcons.Core.Helpers;
+internal static class EnumHelper
+{
+    internal static string GetEnumDescription(this Enum enumValue)
+    {
+        FieldInfo field = enumValue.GetType().GetField(enumValue?.ToString());
+        if (Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is DescriptionAttribute attribute)
+        {
+            return attribute.Description;
+        }
+        return string.Empty;
+    }
+    internal static T GetEnumValueByDescription<T>(this string description) where T : Enum
+    {
+        foreach (Enum enumItem in Enum.GetValues(typeof(T)))
+        {
+            if (enumItem.GetEnumDescription() == description)
+            {
+                return (T)enumItem;
+            }
+        }
+        return default;
+    }
+
+
+}
