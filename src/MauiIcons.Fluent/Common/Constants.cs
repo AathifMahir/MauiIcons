1b78019938087c588727261d4574f9b2c87546bd
Aathif_Mahir <aathifmahir@gmail.com>
2022-11-02 23:06:18 +0530

    Fluent Constants

---
+++ src/MauiIcons.Fluent/Common/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace MauiIcons.Fluent;
+internal sealed class Constants
+{
+    // Font Family
+    public static readonly string FontFamily = "FluentIcons";
+    // Font Asset TTF
+    public static readonly string TtfFontFamily = "Fluent_Icons.ttf";
+}
