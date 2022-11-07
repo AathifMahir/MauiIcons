7b460310ccdd38f92bd1ff7527cdf4878967c051
Aathif_Mahir <aathifmahir@gmail.com>
2022-11-07 07:45:27 +0530

    New ThemeHelper for Selecting Default Icon Color

---
+++ src/MauiIcons.Core/Helpers/ThemeHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiIcons.Core.Helpers;
+internal sealed class ThemeHelper
+{
+    internal static Color SetDefaultIconColor()
+    {
+        return Application.Current.RequestedTheme == AppTheme.Dark ? Colors.White 
+            : Application.Current.RequestedTheme == AppTheme.Light ? Colors.Black 
+            : Colors.White;
+    }
+}
