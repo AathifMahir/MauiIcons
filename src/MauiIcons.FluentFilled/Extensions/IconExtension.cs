13d259e27667293045f14fe7d692130087db0124
Aathif_Mahir <aathifmahir@gmail.com>
2023-01-10 04:38:59 +0530

    Icon Extension

---
+++ src/MauiIcons.FluentFilled/Extensions/IconExtension.cs
@@ -0,0 +1,13 @@
+﻿using MauiIcons.Core;
+using MauiIcons.FluentFilled.Common;
+
+namespace MauiIcons.FluentFilled;
+public sealed class IconExtension : BaseIconExtension
+{
+    public new FluentFilledIcons? Icon
+    {
+        get => (FluentFilledIcons?)base.Icon;
+        set => base.Icon = value;
+    }
+    protected override string IconFontFamily { get; set; } = Constants.FontFamily;
+}
