b37ddb912f26a37ad514e366539bc1e4334dd7ae
Aathif_Mahir <aathifmahir@gmail.com>
2023-01-10 04:39:07 +0530

    Common

---
+++ src/MauiIcons.FluentFilled/Common/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace MauiIcons.FluentFilled.Common;
+internal sealed class Constants
+{
+    // Font Family
+    internal static readonly string FontFamily = "FluentFilledIcons";
+    // Font Asset TTF
+    internal static readonly string TtfFontFamily = "FluentSystemIcons-Filled.ttf";
+}
