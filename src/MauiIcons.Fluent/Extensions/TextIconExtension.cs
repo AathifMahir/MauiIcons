fa0e9b50080faff2d30bb8a78a3ba7b27bd01161
Aathif_Mahir <aathifmahir@gmail.com>
2023-06-11 22:16:01 +0530

    Fluent Text Icon Extension

---
+++ src/MauiIcons.Fluent/Extensions/TextIconExtension.cs
@@ -0,0 +1,12 @@
+﻿using MauiIcons.Core;
+
+namespace MauiIcons.Fluent;
+public sealed class TextIconExtension : BaseTextIconExtension
+{
+    public new FluentIcons? Icon
+    {
+        get => (FluentIcons?)base.Icon;
+        set => base.Icon = value;
+    }
+    protected override string IconFontFamily { get; set; } = Constants.FontFamily;
+}
