4501bacddace8ad05d236e4b800a0008cc2a473e
Aathif_Mahir <aathifmahir@gmail.com>
2023-06-11 22:16:23 +0530

    Material Text Icon Extension

---
+++ src/MauiIcons.Material/Extensions/TextIconExtension.cs
@@ -0,0 +1,12 @@
+﻿using MauiIcons.Core;
+
+namespace MauiIcons.Material;
+public sealed class TextIconExtension : BaseTextIconExtension
+{
+    public new MaterialIcons? Icon
+    {
+        get => (MaterialIcons?)base.Icon;
+        set => base.Icon = value;
+    }
+    protected override string IconFontFamily { get; set; } = Constants.FontFamily;
+}
