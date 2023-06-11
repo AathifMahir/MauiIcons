2fa0779c289e922391f966ebe1c61aab999aaa52
Aathif_Mahir <aathifmahir@gmail.com>
2023-06-11 22:15:48 +0530

    Base Text Icon Extension

---
+++ src/MauiIcons.Core/Extensions/BaseTextIconExtension.cs
@@ -0,0 +1,17 @@
+﻿using MauiIcons.Core.Helpers;
+
+namespace MauiIcons.Core;
+public abstract class BaseTextIconExtension : IMarkupExtension<string>
+{
+    public virtual Enum Icon { get; set; }
+    protected abstract string IconFontFamily { get; set; }
+    public string ProvideValue(IServiceProvider serviceProvider)
+    {
+        return Icon != null ? EnumHelper.GetEnumDescription(Icon) : string.Empty;
+    }
+
+    object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+    {
+        return (this as IMarkupExtension<string>).ProvideValue(serviceProvider);
+    }
+}
