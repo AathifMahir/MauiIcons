8d9b91c6107e48b52d539fb947de694ee0a83064
Aathif_Mahir <aathifmahir@gmail.com>
2023-06-16 12:30:48 +0530

    Unified Icon and IconText Extension Into Single Markup Extension

--- src/MauiIcons.Core/Extensions/BaseIconExtension.cs
+++ src/MauiIcons.Core/Extensions/BaseIconExtension.cs
@@ -1,22 +1,60 @@
 ﻿using MauiIcons.Core.Helpers;
 
 namespace MauiIcons.Core;
-public abstract class BaseIconExtension : IMarkupExtension<ImageSource>
+
+[ContentProperty(nameof(Icon))]
+public abstract class BaseIconExtension : IMarkupExtension<object>
 {
     public virtual Enum Icon { get; set; }
     public Color IconColor { get; set; }
     public double IconSize { get; set; } = 30.0;
     public bool IconAutoScaling { get; set; }
     protected abstract string IconFontFamily { get; set; }
-    public ImageSource ProvideValue(IServiceProvider serviceProvider)
+    public object ProvideValue(IServiceProvider serviceProvider)
     {
-        return new FontImageSource() { Glyph = Icon is not null ? EnumHelper.GetEnumDescription(Icon) : string.Empty, 
-            Color = IconColor ?? ThemeHelper.SetDefaultIconColor(), FontFamily = IconFontFamily, Size = IconSize, 
-            FontAutoScalingEnabled = IconAutoScaling };
+        IProvideValueTarget provideValueTarget = serviceProvider.GetService(typeof(IProvideValueTarget)) as IProvideValueTarget;
+        Type returnType = (provideValueTarget.TargetProperty as BindableProperty)?.ReturnType;
+
+        if (returnType == typeof(string))
+        {
+            AssignFontFamily(provideValueTarget.TargetObject);
+            return Icon is not null ? EnumHelper.GetEnumDescription(Icon) : string.Empty;
+        }
+        if(returnType == typeof(ImageSource))
+        {
+            return new FontImageSource()
+            {
+                Glyph = Icon is not null ? EnumHelper.GetEnumDescription(Icon) : string.Empty,
+                Color = IconColor ?? ThemeHelper.SetDefaultIconColor(),
+                FontFamily = IconFontFamily,
+                Size = IconSize,
+                FontAutoScalingEnabled = IconAutoScaling
+            };
+        }
+        throw new NotSupportedException($"Icon Extension Doesn't Support {returnType}");
     }
 
-    object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+    public void AssignFontFamily(object targetObject)
     {
-        return (this as IMarkupExtension<ImageSource>).ProvideValue(serviceProvider);
+        switch (targetObject)
+        {
+            case Button button:
+                button.FontFamily = IconFontFamily;
+                break;
+            case Label label:
+                label.FontFamily = IconFontFamily;
+                break;
+            case Entry entry:
+                entry.FontFamily = IconFontFamily;
+                break;
+            case Editor editor:
+                editor.FontFamily = IconFontFamily;
+                break;
+            case SearchBar searchBar:
+                searchBar.FontFamily = IconFontFamily;
+                break;
+            default:
+                throw new NotSupportedException($"Icon Extension Doesn't Support this Control {targetObject}");
+        }
     }
 }
