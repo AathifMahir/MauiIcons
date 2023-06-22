519d49f655379de97a8c57f0d6f58cbee0a20677
Aathif_Mahir <aathifmahir@gmail.com>
2023-06-22 12:34:37 +0530

    Constants and Builder Extension

---
+++ src/MauiIcons.Cupertino/Common/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace MauiIcons.Cupertino;
+internal sealed class Constants
+{
+    // Font Family
+    internal static readonly string FontFamily = "CupertinoIcons";
+    // Font Asset TTF
+    internal static readonly string TtfFontFamily = "Cupertino_Icons.ttf";
+}
