1e0dc6bb686525b3e894d94abbb0b3a849594fc0
Aathif_Mahir <aathifmahir@gmail.com>
2023-06-29 11:17:55 +0530

    New BaseIconVariantExtension

---
+++ src/MauiIcons.Core/Extensions/BaseIconVariantExtension.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MauiIcons.Core;
+public abstract class BaseIconVariantExtension : BaseIconExtension, INotifyPropertyChanged
+{
+    Enum _variant;
+    public virtual Enum Variant 
+    {
+        get => _variant;
+        set
+        {
+            if (value != _variant)
+            {
+                _variant = value;
+                IconFontFamily = NotifyVariantChanges();
+                OnPropertyChanged();
+            }
+        }
+    }
+    protected abstract Dictionary<Enum, string> VariantType { get; set; }
+
+    string _iconFontFamily;
+    protected override string IconFontFamily 
+    {
+        get => _iconFontFamily; 
+        set
+        {
+            if (value != _iconFontFamily)
+            {
+                _iconFontFamily = value;
+                OnPropertyChanged();
+            }
+        } 
+    }
+
+    public string NotifyVariantChanges()
+    {
+        if (VariantType is null) return default;
+        if (!VariantType.ContainsKey(Variant)) return default;
+        VariantType.TryGetValue(Variant, out string fontFamily);
+        return fontFamily;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string name = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
