6171f3efdda7dc679fcd490836d23f5ea100ed21
Aathif_Mahir <aathifmahir@gmail.com>
2023-10-25 23:24:45 +0530

    Improved Cupertino

---
+++ src/MauiIcons.Cupertino/AssemblyInfo.shared.cs
@@ -0,0 +1,12 @@
+﻿[assembly: XmlnsDefinition(Constants.XamlNamespace, Constants.MauiIconsNamespacePrefix + nameof(MauiIcons.Cupertino))]
+
+[assembly: Microsoft.Maui.Controls.XmlnsPrefix(Constants.XamlNamespace, "icons")]
+
+static partial class Constants
+{
+    internal const string XamlNamespace = "http://www.aathifmahir.com/dotnet/2022/maui/icons";
+    internal const string MauiIconsNamespacePrefix = $"{nameof(MauiIcons)}.";
+
+    internal static readonly string FontFamily = "CupertinoIcons";
+    internal static readonly string TtfFontFamily = "Cupertino_Icons.ttf";
+}
