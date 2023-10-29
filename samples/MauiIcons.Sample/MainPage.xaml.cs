c1bce9317d35097ac035470b9c446924cede6980
Aathif_Mahir <aathifmahir@gmail.com>
2023-10-29 08:08:21 +0530

    Updated Demo

--- samples/MauiIcons.Sample/MainPage.xaml.cs
+++ samples/MauiIcons.Sample/MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using MauiIcons.Core;
+using MauiIcons.Fluent;
+using MauiIcons.Material;
 
 namespace MauiIcons.Sample;
 
@@ -7,23 +9,25 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
+        // Temporary Workaround for url styled namespace in xaml
         _ = new MauiIcon();
-        //AddToStack();
+        AddToStack();
     }
+    void AddToStack()
+    {
+        var image = new Image() { Source = (ImageSource)new MauiIcon() { Icon = MaterialIcons.AddComment, IconColor = Colors.Lime } };
+        var MauiIcon = new MauiIcon() { Icon = MaterialIcons.AddComment, IconColor = Colors.LightYellow };
+        var MauiIcon1 = new MauiIcon() { Icon = MaterialIcons.AddComment, IconColor = Colors.LightBlue };
+        var MauiIcon2 = new MauiIcon() { Icon = MaterialIcons.AddComment, IconColor = Colors.LightSteelBlue };
+        var MauiIcon3 = new Image() { Source = (ImageSource)new MauiIcon() { Icon = MaterialIcons.AddComment, IconColor = Colors.LimeGreen } };
+        Label MauiIcon4 = (Label)new MauiIcon() { Icon = FluentIcons.Accessibility48, EntranceAnimationType = AnimationType.Rotate, IconSuffix = "New Suffix" };
 
-    //void AddToStack()
-    //{
-    //    var image = new Image() { Source = (ImageSource)new MauiIcon() { Icon = MaterialIcons.AddComment, IconColor = Colors.Lime } };
-    //    var MauiIcon = new MauiIcon() { Icon = MaterialIcons.AddComment, IconColor = Colors.LightYellow };
-    //    var MauiIcon1 = new MauiIcon() { Icon = MaterialIcons.AddComment, IconColor = Colors.LightBlue };
-    //    var MauiIcon2 = new MauiIcon() { Icon = MaterialIcons.AddComment, IconColor = Colors.LightSteelBlue };
-    //    var MauiIcon3 = new Image() { Source = (ImageSource)new MauiIcon() { Icon = MaterialIcons.AddComment, IconColor = Colors.LimeGreen } };
-
-    //    StackMaterialCodeBehind.Add(image);
-    //    StackMaterialCodeBehind.Add(MauiIcon);
-    //    StackMaterialCodeBehind.Add(MauiIcon1);
-    //    StackMaterialCodeBehind.Add(MauiIcon2);
-    //    StackMaterialCodeBehind.Add(MauiIcon3);
-    //}
+        StackMaterialCodeBehind.Add(image);
+        StackMaterialCodeBehind.Add(MauiIcon);
+        StackMaterialCodeBehind.Add(MauiIcon1);
+        StackMaterialCodeBehind.Add(MauiIcon2);
+        StackMaterialCodeBehind.Add(MauiIcon3);
+        StackMaterialCodeBehind.Add(MauiIcon4);
+    }
 }
 
