244bfad0e1ac3409272d78e434f636b6851123d5
Aathif_Mahir <aathifmahir@gmail.com>
2023-10-31 08:42:00 +0530

    Updated Sample

--- samples/MauiIcons.Sample.Markup/MainPage.xaml.cs
+++ samples/MauiIcons.Sample.Markup/MainPage.xaml.cs
@@ -21,25 +21,30 @@ public partial class MainPage : ContentPage
     {
         Content = new VerticalStackLayout()
         {
-
+            HorizontalOptions = LayoutOptions.Center,
+            VerticalOptions = LayoutOptions.Center,
             Children =
             {
                 new HorizontalStackLayout()
                 {
                     Margin = new Thickness(0,12,0,12),
-                    HorizontalOptions = LayoutOptions.Center,
-                    VerticalOptions = LayoutOptions.Center,
+                    Spacing = 15,
                     Children =
                     {
-                         new MauiIcon().Icon(FluentIcons.Accessibility48).IconColor(Colors.Cyan),
+                         new MauiIcon().Icon(FluentFilledIcons.Accessibility48Filled).IconColor(Colors.Cyan),
                          new MauiIcon().Icon(FluentFilledIcons.Accessibility48Filled).IconColor(Colors.Green),
                          new MauiIcon().Icon(MaterialIcons.Accessibility).IconColor(Colors.Blue),
                          new MauiIcon().Icon(MaterialRoundedIcons.Accessibility).IconColor(Colors.Yellow),
                          new MauiIcon().Icon(MaterialOutlinedIcons.Accessibility).IconColor(Colors.Magenta),
                          new MauiIcon().Icon(MaterialSharpIcons.Accessibility).IconColor(Colors.Violet),
                          new MauiIcon().Icon(CupertinoIcons.AntFill).IconColor(Colors.Purple),
-                         new ImageButton().Source((ImageSource)new MauiIcon().Icon(MaterialOutlinedIcons.SmokingRooms)),
-                         
+                         new ImageButton().Icon(MaterialOutlinedIcons.SmokingRooms),
+                         new Image().Icon(FluentIcons.Accessibility24),
+                         new Image().Icon(FluentIcons.Accessibility24).IconColor(Colors.Blue),
+                         new Image().Icon(MaterialIcons.AccessAlarm).IconSize(40.0).IconColor(Colors.Cyan).IconBackgroundColor(Colors.White),
+                         new Label().Icon(FluentIcons.Accessibility24).IconSize(40.0).IconColor(Colors.Red),
+                         new Entry().Icon(FluentIcons.Airplane20).IconSize(20.0).IconColor(Colors.Aqua),
+                         //new Button().Text(CupertinoIcons.Alarm).IconColor(Colors.Red),
                     }
                 }
             }
