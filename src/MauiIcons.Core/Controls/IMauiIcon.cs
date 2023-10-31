319617da45a9e8243e84787cc6fdb240e6fb0613
Aathif_Mahir <aathifmahir@gmail.com>
2023-10-31 08:41:52 +0530

    Improved Interface

--- src/MauiIcons.Core/Controls/IMauiIcon.cs
+++ src/MauiIcons.Core/Controls/IMauiIcon.cs
@@ -3,70 +3,72 @@
 namespace MauiIcons.Core;
 public interface IMauiIcon
 {
+
 #nullable enable
-    public Enum? Icon 
-    { 
-        get; 
-    }
+    /// <summary>
+    /// Gets or sets the icon enum value.
+    /// </summary>
+    Enum? Icon { get; }
 
 #nullable disable
 
+    /// <summary>
+    /// Gets or sets the size of the icon.
+    /// </summary>
     [System.ComponentModel.TypeConverter(typeof(FontSizeConverter))]
-    public double IconSize
-    {
-        get;
-    }
+    double IconSize { get; }
 
+    /// <summary>
+    /// Gets or sets the color of the icon.
+    /// </summary>
     [System.ComponentModel.TypeConverter(typeof(ColorTypeConverter))]
-    public Color IconColor
-    {
-        get;
-    }
+    Color IconColor { get; }
 
+    /// <summary>
+    /// Gets or sets the background color of the icon.
+    /// </summary>
     [System.ComponentModel.TypeConverter(typeof(ColorTypeConverter))]
-    public Color IconBackgroundColor
-    {
-        get;
-    }
-    public bool IconAutoScaling
-    {
-        get;
-    }
-    public string IconSuffix
-    {
-        get;
-    }
+    Color IconBackgroundColor { get; }
 
-    public string IconSuffixFontFamily
-    {
-        get;
-    }
+    /// <summary>
+    /// Gets or sets a value indicating whether the icon should automatically scale.
+    /// </summary>
+    bool IconAutoScaling { get; }
 
+    /// <summary>
+    /// Gets or sets the suffix for the icon.
+    /// </summary>
+    string IconSuffix { get; }
+
+    /// <summary>
+    /// Gets or sets the font family for the icon suffix.
+    /// </summary>
+    string IconSuffixFontFamily { get; }
+
+    /// <summary>
+    /// Gets or sets the font size for the icon suffix.
+    /// </summary>
     [System.ComponentModel.TypeConverter(typeof(FontSizeConverter))]
-    public double IconSuffixFontSize
-    {
-        get;
+    double IconSuffixFontSize { get; }
 
-    }
-
+    /// <summary>
+    /// Gets or sets the text color for the icon suffix.
+    /// </summary>
     [System.ComponentModel.TypeConverter(typeof(ColorTypeConverter))]
-    public Color IconSuffixTextColor
-    {
-        get;
-    }
+    Color IconSuffixTextColor { get; }
 
-    public bool IconSuffixAutoScaling
-    {
-        get;
-    }
+    /// <summary>
+    /// Gets or sets a value indicating whether the icon suffix should automatically scale.
+    /// </summary>
+    bool IconSuffixAutoScaling { get; }
 
-    public AnimationType EntranceAnimationType
-    {
-        get;
-    }
+    /// <summary>
+    /// Gets the type of entrance animation for the element.
+    /// </summary>
+    AnimationType EntranceAnimationType { get; }
 
-    public uint EntranceAnimationDuration
-    {
-        get;
-    }
+    /// <summary>
+    /// Gets the duration of the entrance animation for the element.
+    /// </summary>
+    uint EntranceAnimationDuration { get; }
 }
