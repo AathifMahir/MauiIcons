caaeb2763ccd0a5e0f302549ae49179d234d3cbc
Aathif_Mahir <aathifmahir@gmail.com>
2023-10-31 08:41:43 +0530

    Added New Exception

---
+++ src/MauiIcons.Core/Exceptions/MauiIconsExpection.cs
@@ -0,0 +1,6 @@
+﻿namespace MauiIcons.Core;
+public sealed class MauiIconsExpection : Exception
+{
+    public MauiIconsExpection(string message): base(message){}
+    public MauiIconsExpection(string code, string message) : base(string.Format("{code}: {message}", code, message)){}
+}
