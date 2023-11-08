995130e018bb9a838b09128ea575e752da58894e
Aathif_Mahir <aathifmahir@gmail.com>
2023-11-09 01:22:49 +0530

    New PlatformHelper

---
+++ src/MauiIcons.Core/Helpers/PlatformHelper.cs
@@ -0,0 +1,39 @@
+﻿namespace MauiIcons.Core.Helpers;
+internal static class PlatformHelper
+{
+    public static bool IsValidPlatform(PlatformType platform)
+    {
+        var devicePlatform = DeviceInfo.Platform;
+        var currentPlatform = Constants.PlatformMapping.ContainsKey(devicePlatform) ? Constants.PlatformMapping[devicePlatform] : PlatformType.All;
+
+        if (platform is PlatformType.All || currentPlatform == platform)
+            return true;
+
+        return false;
+    }
+
+    public static bool IsValidIdiom(IdiomType idiom)
+    {
+        var deviceIdiom = DeviceInfo.Idiom;
+        var currentIdiom = Constants.IdiomMapping.ContainsKey(deviceIdiom) ? Constants.IdiomMapping[deviceIdiom] : IdiomType.All;
+
+        if (idiom is IdiomType.All || currentIdiom == idiom)
+            return true;
+
+        return false;
+    }
+
+    public static bool IsValidPlatformAndIdiom(PlatformType platform, IdiomType idiom)
+    {
+        var devicePlatform = DeviceInfo.Platform;
+        var deviceIdiom = DeviceInfo.Idiom;
+        var currentPlatform = Constants.PlatformMapping.ContainsKey(devicePlatform) ? Constants.PlatformMapping[devicePlatform] : PlatformType.All;
+        var currentIdiom = Constants.IdiomMapping.ContainsKey(deviceIdiom) ? Constants.IdiomMapping[deviceIdiom] : IdiomType.All;
+
+        if ((platform is PlatformType.All || currentPlatform == platform) &&
+        (idiom is IdiomType.All || currentIdiom == idiom))
+            return true;
+
+        return false;
+    }
+}
