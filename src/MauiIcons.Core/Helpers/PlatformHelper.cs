e31ab49ed9df6347396e4584dc07b10712ef3b88
Aathif_Mahir <aathifmahir@gmail.com>
2023-11-14 02:16:50 +0530

    Improved Platform Helper

--- src/MauiIcons.Core/Helpers/PlatformHelper.cs
+++ src/MauiIcons.Core/Helpers/PlatformHelper.cs
@@ -1,78 +1,82 @@
 ﻿namespace MauiIcons.Core.Helpers;
 internal static class PlatformHelper
 {
-    public static bool IsValidPlatform(PlatformType platform)
-    {
-        var devicePlatform = DeviceInfo.Platform;
-        var currentPlatform = Constants.PlatformMapping.ContainsKey(devicePlatform) ? Constants.PlatformMapping[devicePlatform] : PlatformType.All;
-
-        if (platform is PlatformType.All || currentPlatform == platform)
-            return true;
-
-        return false;
-    }
-
     public static bool IsValidPlatform(IList<string> platforms)
     {
-        foreach(var value in platforms)
+        if (platforms is null) return true;
+
+        // Avoided Using Linq in this scenerio to free up the small difference in memory usage that we measured between this implementation and linq, Since this code runs on startup of the app
+
+        string targetPlatform = DeviceInfo.Platform.ToString();
+
+        foreach (var value in platforms)
         {
-            if (value.Contains(DeviceInfo.Platform.ToString()))
+            if (value.Contains(targetPlatform))
                 return true;
         }
-        return false;
-    }
-
-    public static bool IsValidIdiom(IdiomType idiom)
-    {
-        var deviceIdiom = DeviceInfo.Idiom;
-        var currentIdiom = Constants.IdiomMapping.ContainsKey(deviceIdiom) ? Constants.IdiomMapping[deviceIdiom] : IdiomType.All;
-
-        if (idiom is IdiomType.All || currentIdiom == idiom)
-            return true;
-
         return false;
     }
 
     public static bool IsValidIdiom(IList<string> idioms)
     {
+        if(idioms is null) return true;
+
+        // Avoided Using Linq in this scenerio to free up the small difference in memory usage that we measured between this implementation and linq, Since this code runs on startup of the app
+
+        string targetIdiom = DeviceInfo.Idiom.ToString();
+
         foreach (var value in idioms)
         {
-            if (value.Contains(DeviceInfo.Idiom.ToString()))
+            if (value.Contains(targetIdiom))
                 return true;
         }
         return false;
     }
 
-    public static bool IsValidPlatformAndIdiom(PlatformType platform, IdiomType idiom)
+    public static bool IsValidPlatformsAndIdioms(IList<string> onPlatforms, IList<string> onIdioms)
     {
-        var devicePlatform = DeviceInfo.Platform;
-        var deviceIdiom = DeviceInfo.Idiom;
-        var currentPlatform = Constants.PlatformMapping.ContainsKey(devicePlatform) ? Constants.PlatformMapping[devicePlatform] : PlatformType.All;
-        var currentIdiom = Constants.IdiomMapping.ContainsKey(deviceIdiom) ? Constants.IdiomMapping[deviceIdiom] : IdiomType.All;
+        bool isPlatformsPresent = onPlatforms is not null;
+        bool isIdiomsPresent = onIdioms is not null;
 
-        if ((platform is PlatformType.All || currentPlatform == platform) &&
-        (idiom is IdiomType.All || currentIdiom == idiom))
-            return true;
+        bool isPlatformAndIdiomDefault = !isPlatformsPresent && !isIdiomsPresent;
+        bool isPlatformsAndIdioms = isPlatformsPresent && isIdiomsPresent && IsValidPlatformAndIdiom(onPlatforms, onIdioms);
+        bool isPlatformsOnly = (isPlatformsPresent && !isIdiomsPresent) && IsValidPlatform(onPlatforms);
+        bool isIdiomsOnly = (isIdiomsPresent && !isPlatformsPresent) && IsValidIdiom(onIdioms);
+        
 
-        return false;
+        return isPlatformAndIdiomDefault 
+            || isPlatformsAndIdioms
+            || isPlatformsOnly
+            || isIdiomsOnly;
     }
 
-    public static bool IsValidPlatformAndIdiom(IList<string> platforms, IList<string> idioms)
+    private static bool IsValidPlatformAndIdiom(IList<string> platforms, IList<string> idioms)
     {
         int indexMin = Math.Min(platforms.Count, idioms.Count);
         int indexMax = Math.Max(platforms.Count, idioms.Count);
 
+        bool onPlatform = false;
+        bool onIdiom = false;
+
+        string targetPlatform = DeviceInfo.Platform.ToString();
+        string targetIdiom = DeviceInfo.Idiom.ToString();
+
+        // Avoided Using Linq in this scenerio to free up the small difference in memory usage that we measured between this implementation and linq, Since this code runs on startup of the app
+
         for (int i = 0; i < indexMax; i++)
         {
-            if (i <= indexMin && platforms[i].Contains(DeviceInfo.Platform.ToString()) && idioms[i].Contains(DeviceInfo.Idiom.ToString()))
+            if (i < indexMin && platforms[i].Contains(targetPlatform) && idioms[i].Contains(targetIdiom))
                 return true;
 
-            if (i <= platforms.Count && platforms[i].Contains(DeviceInfo.Platform.ToString()))
-                return true;
+            if (i < platforms.Count && platforms[i].Contains(targetPlatform))
+                onPlatform = true;
 
-            if (i <= idioms.Count && idioms[i].Contains(DeviceInfo.Idiom.ToString()))
-                return true;
+            if (i < idioms.Count && idioms[i].Contains(targetIdiom))
+                onIdiom = true;
+
+            if (onPlatform && onIdiom)
+                break;
         }
-        return false;
+        return onPlatform && onIdiom;
     }
 }
