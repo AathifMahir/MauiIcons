a54fa9ec3b8c0751990b895d65a3e59551dd4662
Aathif_Mahir <aathifmahir@gmail.com>
2023-12-23 18:20:40 +0530

    Assembly Info and Builder Extensions

---
+++ src/MauiIcons.FontAwesome/AssemblyInfo.shared.cs
@@ -0,0 +1,12 @@
+﻿[assembly: XmlnsDefinition(Constants.XamlNamespace, Constants.MauiIconsNamespacePrefix + nameof(MauiIcons.FontAwesome))]
+
+[assembly: Microsoft.Maui.Controls.XmlnsPrefix(Constants.XamlNamespace, "icons")]
+
+static class Constants
+{
+    internal const string XamlNamespace = "http://www.aathifmahir.com/dotnet/2022/maui/icons";
+    internal const string MauiIconsNamespacePrefix = $"{nameof(MauiIcons)}.";
+
+    internal static readonly string FontFamily = "FontAwesomeIcons";
+    internal static readonly string TtfFontFamily = "FontAwesome6Free-Regular-400.otf";
+}
