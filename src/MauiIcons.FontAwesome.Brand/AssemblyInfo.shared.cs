7fa87f2a1bd0ba4f1b81fa9e84b9e8c721a9f3f1
Aathif_Mahir <aathifmahir@gmail.com>
2023-12-23 20:32:53 +0530

    Brand Assembly and Builder Extension

---
+++ src/MauiIcons.FontAwesome.Brand/AssemblyInfo.shared.cs
@@ -0,0 +1,14 @@
+﻿[assembly: XmlnsDefinition(Constants.XamlNamespace, Constants.MauiIconsNamespacePrefix + nameof(MauiIcons.FontAwesome.Brand))]
+[assembly: XmlnsDefinition(Constants.XamlNamespace, Constants.MauiIconsNamespace)]
+
+[assembly: Microsoft.Maui.Controls.XmlnsPrefix(Constants.XamlNamespace, "icons")]
+
+static class Constants
+{
+    internal const string XamlNamespace = "http://www.aathifmahir.com/dotnet/2022/maui/icons";
+    internal const string MauiIconsNamespace = $"{nameof(MauiIcons)}.{nameof(MauiIcons.FontAwesome)}";
+    internal const string MauiIconsNamespacePrefix = $"{MauiIconsNamespace}.";
+
+    internal static readonly string FontFamily = "FontAwesomeBrandIcons";
+    internal static readonly string TtfFontFamily = "FontAwesome6Brands-Regular-400.otf";
+}
