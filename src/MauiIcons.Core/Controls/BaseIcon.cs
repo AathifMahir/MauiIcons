21eba53e86cb9b7264e81e9293608f000c2c981c
Aathif_Mahir <aathifmahir@gmail.com>
2024-07-20 14:55:18 +0530

    New BaseIcon

---
+++ src/MauiIcons.Core/Controls/BaseIcon.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Maui.Graphics.Converters;
+
+namespace MauiIcons.Core;
+public class BaseIcon : ContentView
+{
+    public static readonly BindableProperty IconProperty = BindableProperty.Create(nameof(Icon), typeof(Enum), typeof(BaseIcon), null);
+    public static readonly BindableProperty IconSizeProperty = BindableProperty.Create(nameof(IconSize), typeof(double), typeof(BaseIcon), 30.0);
+    public static readonly BindableProperty IconColorProperty = BindableProperty.Create(nameof(IconColor), typeof(Color), typeof(BaseIcon), null);
+    public static readonly BindableProperty IconBackgroundColorProperty = BindableProperty.Create(nameof(IconBackgroundColor), typeof(Color), typeof(BaseIcon), null);
+    public static readonly BindableProperty IconAutoScalingProperty = BindableProperty.Create(nameof(IconAutoScaling), typeof(bool), typeof(BaseIcon), false);
+
+    public Enum? Icon
+    {
+        get => (Enum?)GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+
+    [System.ComponentModel.TypeConverter(typeof(FontSizeConverter))]
+    public double IconSize
+    {
+        get => (double)GetValue(IconSizeProperty);
+        set => SetValue(IconSizeProperty, value);
+    }
+
+    [System.ComponentModel.TypeConverter(typeof(ColorTypeConverter))]
+    public Color IconColor
+    {
+        get => (Color)GetValue(IconColorProperty);
+        set => SetValue(IconColorProperty, value);
+    }
+
+    [System.ComponentModel.TypeConverter(typeof(ColorTypeConverter))]
+    public Color IconBackgroundColor
+    {
+        get => (Color)GetValue(IconBackgroundColorProperty);
+        set => SetValue(IconBackgroundColorProperty, value);
+    }
+
+    public bool IconAutoScaling
+    {
+        get => (bool)GetValue(IconAutoScalingProperty);
+        set => SetValue(IconAutoScalingProperty, value);
+    }
+
+
+}
