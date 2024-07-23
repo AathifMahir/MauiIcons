cb14e865e6d664ab65e38c21c9c228f17bde34b9
Aathif_Mahir <aathifmahir@gmail.com>
2024-07-23 08:22:22 +0530

    Base Impl

---
+++ src/MauiIcons.Core/Base/BaseMauiIcon.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiIcons.Core.Base;
+public abstract class BaseMauiIcon : ContentView
+{
+    public static readonly BindableProperty IconProperty = BindableProperty.Create(nameof(Icon), typeof(Enum), typeof(BaseMauiIcon), null);
+
+    public Enum? Icon
+    {
+        get => (Enum?)GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+}
