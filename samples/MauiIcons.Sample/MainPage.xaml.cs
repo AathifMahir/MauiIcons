fadf7b5a664683812e287b5a2ab05ce892c7e5f4
Aathif Mahir <aathifmahir@gmail.com>
2024-09-25 11:37:03 +0530

    Sample Update

--- samples/MauiIcons.Sample/MainPage.xaml.cs
+++ samples/MauiIcons.Sample/MainPage.xaml.cs
@@ -9,9 +9,10 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
+        BindingContext = this;
         // Temporary Workaround for url styled namespace in xaml
         _ = new MauiIcon();
-        BindingContext = this;
+
     }
 
     private CupertinoIcons _icon = CupertinoIcons.Airplane;
@@ -31,8 +32,8 @@ public partial class MainPage : ContentPage
 
     private FontAwesomeBrandIcons _icon1 = FontAwesomeBrandIcons.Microsoft;
 
-    public FontAwesomeBrandIcons MyIcon1 
-    { 
+    public FontAwesomeBrandIcons MyIcon1
+    {
         get => _icon1;
         set
         {
@@ -45,8 +46,8 @@ public partial class MainPage : ContentPage
     }
 
     private Color _labelColor = Colors.Coral;
-    public Color MyLabelColor 
-    { 
+    public Color MyLabelColor
+    {
         get => _labelColor;
         set
         {
@@ -58,11 +59,72 @@ public partial class MainPage : ContentPage
         }
     }
 
+    bool _isPlaceHolder = true;
+    public bool IsPlaceHolder
+    {
+        get => _isPlaceHolder;
+        set
+        {
+            if (value != _isPlaceHolder)
+            {
+                _isPlaceHolder = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    IList<string> _onPlatforms = ["WinUI", "Android"];
+    public IList<string> OnPlatforms
+    {
+        get => _onPlatforms;
+        set
+        {
+            if (value != _onPlatforms)
+            {
+                _onPlatforms = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    IList<string> _onIdioms = ["Desktop", "Mobile"];
+    public IList<string> OnIdioms
+    {
+        get => _onIdioms;
+        set
+        {
+            if (value != _onIdioms)
+            {
+                _onIdioms = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private string _myPlaceHolder = "This is a Placeholder";
+
+    public string MyPlaceHolder
+    {
+        get => _myPlaceHolder;
+        set
+        {
+            if (value != _myPlaceHolder)
+            {
+                _myPlaceHolder = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
     private void Button_Clicked(object sender, EventArgs e)
     {
         MyIcon = MyIcon == CupertinoIcons.Airplane ? CupertinoIcons.Alarm : CupertinoIcons.Airplane;
         MyIcon1 = MyIcon1 == FontAwesomeBrandIcons.Microsoft ? FontAwesomeBrandIcons.Apple : FontAwesomeBrandIcons.Microsoft;
         MyLabelColor = MyLabelColor == Colors.Coral ? Colors.Blue : Colors.Coral;
+        IsPlaceHolder = !IsPlaceHolder;
+        OnPlatforms = OnPlatforms.Contains("WinUI") ? ["Android"] : ["WinUI", "Android"];
+        OnIdioms = OnIdioms.Contains("Desktop") ? ["Mobile"] : ["Desktop", "Mobile"];
+        MyPlaceHolder = MyPlaceHolder == "This is a Placeholder" ? "This is a New Placeholder" : "This is a Placeholder";
     }
 }
 
